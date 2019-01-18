d9a870a5d9a703706ab81da61d469c5f8e1a064e
Dariel Urbina <durbinajuan@outlook.com>
2019-01-18 09:14:35 -0500

    working on the cdn in the bundle config

--- PersonalWebSite/App_Start/BundleConfig.cs
+++ PersonalWebSite/App_Start/BundleConfig.cs
@@ -6,18 +6,30 @@ namespace PersonalWebSite.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js",
-                "~/Scripts/jquery.validate*"));           
+#if DEBUG
+            BundleTable.EnableOptimizations = true;
+            BundleTable.Bundles.UseCdn = true;
+#endif
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jquery",
+                "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.0.0.min.js").Include(
+                "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap",
+                "https://ajax.aspnetcdn.com/ajax/bootstrap/3.0.0/bootstrap.min.js").Include(
                 "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/fontawesome",
+                "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css").Include(
+                "~/Content/font-awesome.css"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
                 "~/Content/site.css",
-                "~/Content/messages.css",
-                "~/Content/font-awesome.css"));
+                "~/Content/messages.css"));
         }
     }
 }
